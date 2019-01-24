f6e4af5333f22d53df8a34fb8fe93b9578c01a1c
Hiroshi Fujiwara <hiroshi.829f@gmail.com>
2019-01-24 09:44:28 +0000

    chapter04 added

---
+++ chapter04/work06.js
@@ -0,0 +1,5 @@
+var num1 = 5;
+var num2 = 3;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
---
+++ chapter04/work07.js
@@ -0,0 +1,6 @@
+var num1 = 10;
+var num2 = 2;
+var num3 = 3;
+
+console.log( num1 / num2 - num3);
+console.log( num3 * num2 - num1);

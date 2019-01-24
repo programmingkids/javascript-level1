c10a3451589158d094b3f4372cf931472bc2296d
Hiroshi Fujiwara <hiroshi.829f@gmail.com>
2019-01-24 07:02:08 +0000

    chapter03 added

---
+++ chapter03/work03.js
@@ -0,0 +1,3 @@
+console.log(4 * 3 + 2);
+console.log((18 / 3) - 1);
+console.log((4 - 2) * (8 - 3));
---
+++ chapter03/work04.js
@@ -0,0 +1,3 @@
+console.log(5 * (5 - 2));
+console.log(10 / (4 - 2));
+console.log((5 * 3) / (3 + 2));

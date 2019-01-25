5499da65a7233fa4e40cbab307d25077ae1de16c
Hiroshi Fujiwara <hiroshi.829f@gmail.com>
2019-01-25 04:32:59 +0000

    chapter05 added

---
+++ chapter05/work03.js
@@ -0,0 +1,16 @@
+var a = 10;
+
+a += 2;
+console.log( a ); // 12
+
+a -= 2;
+console.log( a ); // 10
+
+a *= 2;
+console.log( a ); // 20
+
+a /= 2;
+console.log( a ); // 10
+
+a %= 2;
+console.log( a ); // 0
---
+++ chapter05/work04.js
@@ -0,0 +1,7 @@
+var a = 10;
+
+a++; // aを1だけ増加させる
+console.log( a ); // 11
+
+a--; // aを1だけ減少させる
+console.log( a ); // 10

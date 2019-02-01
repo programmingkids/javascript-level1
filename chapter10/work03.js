368046260e35a2a5cbb56549f3c7008435d7c344
Hiroshi Fujiwara <hiroshi.829f@gmail.com>
2019-02-01 02:23:30 +0000

    fix chapter10

--- chapter10/work03.js
+++
@@ -1,5 +0,0 @@
-var person = { name:"山田", age:17, hobby:"tennis" };
-
-console.log( person["name"] );
-console.log( person["age"] );
-console.log( person["hobby"] );
--- chapter10/work06.js
+++
@@ -1,9 +0,0 @@
-var person = {
-    name:"山田", 
-    age:17, 
-    hobby:"tennis",
-};
-
-for ( var key in person ) {
-    console.log(key + " ==> " + person[key]);
-}
